163c164638aa17a37d1127896fbc22eb7f78e6a0
JetsadaWijit <56456776+JetsadaWijit@users.noreply.github.com>
2024-04-20 07:42:49 +0000

    set up

---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+void setup() {
+    // No serial initialization needed for console output
+}
+
+void loop() {
+    std::cout << "Loop!" << std::endl;  // Or printf("Loop!\n");
+}
