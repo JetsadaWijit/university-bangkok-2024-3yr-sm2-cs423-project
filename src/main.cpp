814e40c4576efe12b9609983f1b64634d54bb327
JetsadaWijit <Ze_ro_owen@hotmail.com>
2024-05-15 11:27:46 +0000

    Fix DB code

--- src/main.cpp
+++ src/main.cpp
@@ -12,6 +12,10 @@ const char* mysql_user = "root";
 const char* mysql_password = "";
 const char* mysql_database = "cs423";
 
+IPAddress serveraddr(192, 168, 1, 2);  // MySQL server IP address
+
+MySQL_Connection conn((Client *)&client);
+
 void setup() {
     Serial.begin(9600);
     createTable();
@@ -36,32 +40,24 @@ void loop() {
     sleep(1000);
 }
 
-// Create mysql Check if table if exist if not then create table name "cs423"
 void createTable() {
-    MySQL_Connection conn((Client *)&client);
-    if (conn.connect(server_addr, 3306, user, password)) {
+    Serial.println("Creating table...");
+    if (conn.connect(mysql_server, mysql_user, mysql_password)) {
         Serial.println("Connected to MySQL server");
         delay(1000);
         MySQL_Cursor *cur_mem = new MySQL_Cursor(&conn);
-        cur_mem->execute("SELECT * FROM cs423");
-        row_values *row = NULL;
-        do {
-            row = cur_mem->get_next_row();
-            if (row != NULL) {
-                Serial.println(row->values[0]);
-            }
-        } while (row != NULL);
+        cur_mem->execute("CREATE TABLE IF NOT EXISTS cs423 (time VARCHAR(50))");
         delete cur_mem;
         conn.close();
     } else {
         Serial.println("Connection failed.");
+        conn.close();
     }
 }
 
-// Create function insert data to table insertData(time)
 void insertData(String time) {
-    MySQL_Connection conn((Client *)&client);
-    if (conn.connect(server_addr, 3306, user, password)) {
+    Serial.println("Inserting data...");
+    if (conn.connect(mysql_server, mysql_user, mysql_password)) {
         Serial.println("Connected to MySQL server");
         delay(1000);
         MySQL_Cursor *cur_mem = new MySQL_Cursor(&conn);
@@ -70,5 +66,6 @@ void insertData(String time) {
         conn.close();
     } else {
         Serial.println("Connection failed.");
+        conn.close();
     }
 }
